032bf2e0eba18da50581f7acd344feeb021c73b5
Dylan <dylanhoungbedji@gmail.com>
2023-01-29 02:20:20 +0100

    I'm going to talk english now. I add the return to stop the program when it's crashing in the launch_game function. There is a return at each exceptions. I manage to open a specific error window for each exceptions. And I will continue tomorow hehehe

--- boumbot.py
+++ boumbot.py
@@ -15,57 +15,48 @@ from tkinter import *
 from tkinter import ttk
 
 
-# Partie sur tkinter et l'interface qui demande a l'utilisateur le code de la sesssion de jeu (XXXX)
-
-# Fonction qui va afficher le code dans la console
-def get_code():
-	c = entry_code.get() # On récupère le code de la partie que l'on veut rejoindre
+# function that will launch the game
+def launch_game():
+	code = entry_code.get() # On récupère le code de la partie que l'on veut rejoindre
 	options = Options()
 	chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 	 
-	chrome.get(f'https://jklm.fun/{c}') # Ouvertur d'une page chrome avec l'url de la partie
+	chrome.get(f'https://jklm.fun/{code}') # Ouverture d'une page chrome avec l'url de la partie
 
 	# Etape de la connexion du bot avec son pseudo 'BoomBot
-	time.sleep(2) # On attend que la page charge
-
 	try:
 		# On attend jusqu'a que l'input pour rentrer son pseudo apparaisse
 		nickName = WebDriverWait(chrome, 10).until(
 			EC.presence_of_element_located((By.XPATH,'/html/body/div[2]/div[3]/form/div[2]/input'))
 		)
 		nickName.clear()
-		nickName.send_keys("BoomBot") # Pseudo du Bot /!\ Peut être modifié si vous voulez vous faire passer flash
+		nickName.send_keys("BoomBot") # Pseudo du Bot /!\ Peut être modifié si vous voulez faire une petite blague à vos amis
 		nickName.send_keys(Keys.RETURN)
 	except ElementNotInteractableException: # Exceptions qui signal si le programme n'à pas réussi à trouver le input 
-		print("Je trouve pas le input")
+		print("Cannot find the input")
+		chrome.close()
+		return -1 # we return -1 to stop the program
 	except NoSuchWindowException: # Exception si le joueur a fermé la fenêtre du jeu
-		print("You have closed the window piece of shit")
+		print("You have closed the window")
+		return -1 # we return -1 to stop the program
 
 	# Si la page charge correctement alors on poursuis
 	try:
 		# On attend que la div game (du jeu) arrive pour passer a la suite 
 		game = WebDriverWait(chrome, 10).until(
-		EC.presence_of_element_located((By.CLASS_NAME, "game"))
+			EC.presence_of_element_located((By.CLASS_NAME, "game"))
 		)
-		print("ya game") # Confirmation de la présence de la div game
+		print("it works, step game div") # Confirmation de la présence de la div game
 	except NoSuchElementException: # Esceptions si on ne trouve pas l'élément
-		print("Cant find this shit")
+		print("Cant find the game div")
 		chrome.close()
+		return -1 # we return -1 to stop the program
 	except NoSuchWindowException: # Exception si le joueur a fermé la fenêtre du jeu
-		print("You have closed the window piece of shit")
-	
-	time.sleep(2) # On attend que la page charge.
+		print("You have closed the window")
+		return -1 # we return -1 to stop the program
 
-	# Action qui va faire TAB 2 fois pour aller sur le bouton 'Rejoindre' puis entrer pour rejoindre
-	actions = ActionChains(chrome)
-	actions.send_keys(Keys.TAB * 2)
-	actions.send_keys(Keys.RETURN)
-	actions.perform()
-	
 
-	time.sleep(17) # On attend le chargement de la partie (15secondes normale + 2secondes de sécurité)
-	
-	# Collecte de la syllabe 
+	time.sleep(2)
 	"""
 	Comme le Jeu se déroule dans un Iframe, j'ai besoin de switcher 
 	la selection des elements sur cet Iframe car ils n'existent pas
@@ -78,48 +69,69 @@ def get_code():
 	"""
 	iframe = chrome.find_element(By.XPATH,'/html/body/div[2]/div[4]/div[1]/iframe')
 	chrome.switch_to.frame(iframe)
-	
-	
 
-	# Boucle while qui va toutes les 10 secondes mettre le mot contenant la syllabe recherché
+	time.sleep(2)
+
+	# If the page load correctly, we join the game
+	try:
+		join = WebDriverWait(chrome, 10).until(
+			EC.presence_of_element_located((By.XPATH,"/html/body/div[2]/div[3]/div[1]/div[1]/button"))
+		)
+		join.click() # We click on the button to join
+	except NoSuchElementException:
+		print("Cannot find the join Button")
+		chrome.close()
+		return -1 # we return -1 to stop the program
+	except NoSuchWindowException:
+		print("You have closed the window")
+		return -1 # we return -1 to stop the program
+
+	# Once we have joined the game, we wait for the game to start
+	time.sleep(17) # We wait 15sec for the game + 2sec to secure the loading
+	
+	# While loop that will send the word every 10 seconds
 	val = 0
-	while val < 12: # On fait la boucle pendant 1 minutes car on met un time sleep de 10 secondes 6 fois
+	while val < 12: # It'll loop during 2 mins because it wait 10 sec 12 times => 10*12 = 120s = 2m
 		try:
 			# localisation de la div syllabe DANS LE IFRAME
 			syllabe = chrome.find_element(By.XPATH,'/html/body/div[2]/div[2]/div[2]/div[2]/div').text
-			# Action qui va écrire le texte est l'entré 
+			# Action qui va écrire le texte et l'entrer 
 			dire = ActionChains(chrome)
 			dire.send_keys(syllabe)
 			dire.send_keys(Keys.RETURN)
 			dire.perform()
 			print("It works")
 		except NoSuchWindowException: # Exception si le joueur a fermé la fenêtre
-			print("You have closed the window piece of shit")
-			chrome.close()
+			print("You have closed the window")
+			return -1 # we return -1 to stop the program
+
 		time.sleep(10) # On attend 10 secondes avant de renvoyer un autre texte
-		val += 1 # On incrémente val pour ne pas avoir de boucle à l'infini
+		val += 1 # On incrémente val pour ne pas avoir de boucle infini
 
+# Partie sur tkinter et l'interface qui demande a l'utilisateur le code de la sesssion de jeu (EX: XXXX)
+# Creation of the main game page
+boomWindow = Tk()
+boomWindow.title("BoumWindow")
+boomWindow.geometry("500x500")
+boomWindow["bg"] = "#403831"
 
-boumWindow = Tk()
-boumWindow.title("BoumWindow")
-boumWindow.geometry("500x500")
-boumWindow["bg"] = "#403831"
+# Creation of the title label  
+title = Label(boomWindow,text="Let's use BoomBot!",bg="#403831",font=("Arial",25),fg="white")
+title.pack(pady=40)
 
-# Création du label de Présentation
-label = Label(boumWindow,text="Let's use BoumBot!",bg="#403831",font=("Arial",25),fg="white")
-label.pack(pady=40)
-
-# Création de l'entree qui va accueillir le code du jeu
-entry_code = Entry(boumWindow)
-entry_code.pack(padx=10,pady=30)
+# Creation of the labelon top of the entry
+entry_label = Label(boomWindow,text="Enter the code",bg="#403831",font=("Arial",15),fg="white")
+entry_label.pack()
+# Creation of the entry
+entry_code = Entry(boomWindow)
+entry_code.pack()
 
 # Création du bouton qui va lancer le programme
-btnGetCode = Button(boumWindow,text="Go !",command = get_code)
-btnGetCode.pack()
+btnGetCode = Button(boomWindow,text="Get started !",command = launch_game)
+btnGetCode.pack(pady=30)
 
 # Création du bouton qui ferme la fenetre
-btnQuit = Button(boumWindow,text="LEAVE", command = boumWindow.destroy)
+btnQuit = Button(boomWindow,text="CLOSE", command = boomWindow.destroy)
 btnQuit.pack(pady=100)
 
-
-boumWindow.mainloop()
+boomWindow.mainloop()
